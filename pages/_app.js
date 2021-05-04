d2f783aab564e2a183be5e7c49118a798b4b1e4c
Wes Cole <wesley.cole87@gmail.com>
2021-05-04 01:08:58 -0400

    Initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,14 @@
-import React from 'react'
-import { prefix } from 'goober-autoprefixer'
-import { setup } from 'goober'
+import React from "react";
+import { prefix } from "goober-autoprefixer";
+import { setup } from "goober";
+
+import "../styles/globals.css";
 
 // goober's needs to know how to render the `styled` nodes.
 // So to let it know, we run the `setup` function with the
 // `createElement` function and prefixer function.
-setup(React.createElement, prefix)
+setup(React.createElement, prefix);
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
